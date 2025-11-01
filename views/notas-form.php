d5378390b7a0c8aea0bee487f544da1b30c1bc9d
Sala D2 <CSalaD2@uniboyaca.edu.co>
2025-11-01 10:31:58 -0500

    ingreso de un css que his falta

--- views/notas-form.php
+++ views/notas-form.php
@@ -32,6 +32,7 @@ if (!empty($materia) && !empty($estudiante)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/forms.css">
     <title><?php echo $titulo; ?></title>
 </head>
 
