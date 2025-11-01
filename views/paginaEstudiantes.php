17a9cf12b474f266dca0e2f401d3af8858cbff67
melisa pinilla <you@example.com>
2025-10-31 20:58:03 -0500

    css de los estudiantes

--- views/paginaEstudiantes.php
+++ views/paginaEstudiantes.php
@@ -14,32 +14,33 @@ $estudiantes = $estudiantesController->getEstudiantes();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estudiantes</title>
     <link rel="stylesheet" href="../public/css/modals.css">
+    <link rel="stylesheet" href="../public/css/estudiantes.css">
 </head>
 <body>
-    <h2>Lista de Estudiantes</h2>
+    <h2>Módulo de estudiantes</h2>
     <br>
     <section class="">
         <div class="">
-            <img class="icono" src="" alt="imagen" />
+            <img class="icono" src="../public/res/local_library.svg" alt="imagen" />
             <div class="id">Nuevo Estudiante</div>
             <div class="name">
                 <a href="estudiantes-form.php">Crear estudiante</a>
             </div>
         </div>
         <?php
-        foreach ($estudiantes as $estudiante) {
+                foreach ($estudiantes as $estudiante) {
             echo '<div class="">';
-            echo '  <img class="icono" src="" alt="imagen" />';
-            echo '  <div class="codigo">Código: ' . $estudiante->get('codigo') . '</div>';
-            echo '  <div class="nombre">Nombre: ' . $estudiante->get('nombre') . '</div>';
-            echo '  <div class="email">Email: ' . $estudiante->get('email') . '</div>';
-            echo '  <div class="programa">Programa: ' . $estudiante->get('nombrePrograma') . '</div>';
+            echo '  <img class="icono" src="../public/res/person_book.svg" alt="imagen" />';
+            echo '  <div class="codigo"><strong>Código:</strong> ' . $estudiante->get('codigo') . '</div>';
+            echo '  <div class="nombre"><strong>Nombre:</strong> ' . $estudiante->get('nombre') . '</div>';
+            echo '  <div class="email"><strong>Email:</strong> ' . $estudiante->get('email') . '</div>';
+            echo '  <div class="programa"><strong>Programa:</strong> ' . $estudiante->get('programa') . '</div>';
             echo '  <div class="btns">';
             echo '      <a href="estudiantes-form.php?cod=' . urlencode($estudiante->get('codigo')) . '">';
-            echo '          <img class="icono" src="" alt="imagen"/>';
+            echo '          <img class="icono" src="../public/res/edit.svg" alt="imagen"/>';
             echo '      </a>';
             echo '      <button onclick="borrarEstudiante(\'' . addslashes($estudiante->get('codigo')) . '\')">';
-            echo '          <img class="icono" src="" alt="imagen"/>';
+            echo '          <img class="icono" src="../public/res/delete.svg" alt="imagen"/>';
             echo '      </button>';
             echo '  </div>';
             echo '</div>';
